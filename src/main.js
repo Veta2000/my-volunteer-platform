d76e776d1157ebb5de40d32485ca86ef1964a115
Veta2000 <elizter121@gmail.com>
2024-09-12 19:12:23 +0300

    слотs

--- src/main.js
+++ src/main.js
@@ -6,14 +6,10 @@ import { createPinia } from 'pinia';
 import piniaPersistedState from 'pinia-plugin-persistedstate';
 
 const app = createApp(App);
-
-// Создаем экземпляр Pinia
 const pinia = createPinia();
 
-// Используем плагин piniaPersistedState
 pinia.use(piniaPersistedState);
 
-// Подключаем Vuetify, Router и Pinia
 app.use(vuetify);
 app.use(router);
 app.use(pinia);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,35 @@
 <template>
-    <v-container>
-      <v-row justify="center">
-        <v-col cols="12" md="8" lg="6" class="text-center">
-          <h1 class="mb-5">Платформа для Организации Волонтерских Мероприятий</h1>
-          <p class="mb-5">
-            Добро пожаловать на платформу! Здесь вы можете найти интересные волонтерские мероприятия, присоединиться к ним и внести свой вклад в важные дела!
-          </p>
-          <v-btn color="primary" @click="goToEvents">
-            Перейти к мероприятиям
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
-  
-  <script setup>
-  import { useRouter } from 'vue-router';
-  
-  const router = useRouter();
-  
-  function goToEvents() {
-    router.push('/events');
-  }
-  </script>
-  
-  <style scoped>
-  
-  </style>
-  
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="8" lg="6" class="text-center">
+        <h1 class="mb-5">Платформа для Организации Волонтерских Мероприятий</h1>
+        <p class="mb-5">
+          Добро пожаловать на платформу! Здесь вы можете найти интересные волонтерские мероприятия, присоединиться к ним и внести свой вклад в важные дела!
+        </p>
+        <v-btn color="primary" @click="goToEvents">Перейти к мероприятиям</v-btn>
+      </v-col>
+    </v-row>
+
+    <!-- Интеграция EventList с кастомным слотом -->
+    <EventList>
+      <template #event-details="{ event }">
+        <v-list-item-title>{{ event.title }} - Изменённое событие</v-list-item-title>
+        <v-list-item-subtitle>{{ event.date }} (дата)</v-list-item-subtitle>
+      </template>
+    </EventList>
+  </v-container>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+import EventList from '../components/EventList.vue'; 
+
+const router = useRouter();
+
+function goToEvents() {
+  router.push('/events');
+}
+</script>
+
+<style scoped>
+</style>
